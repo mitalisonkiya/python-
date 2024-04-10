4fc60e20538174b262797f81704da11bc3840f3a
mitalisonkiya <165659214+mitalisonkiya@users.noreply.github.com>
2024-04-10 12:59:07 +0530

    Update ass4.py

--- python/ass4.py
+++ python/ass4.py
@@ -1,14 +1,1 @@
-class MyClass:
-    class_variable = "Hello, world!"
 
-    def __init__(self, instance_variable):
-        self.instance_variable = instance_variable
-
-# Create an object of MyClass
-obj = MyClass("This is an instance variable.")
-
-# Access class variables using class object
-print("Accessing class variable using class object:", MyClass.class_variable)
-
-# Access instance variable using class object (not recommended)
-print("Accessing instance variable using class object (not recommended):", obj.instance_variable)
